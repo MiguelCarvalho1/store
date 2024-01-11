e126909db53b898524a06653ebe7a68213e8ddb4
migue <miguelangelosilvacarvalho96@gmail.com>
2024-01-11 22:34:22 +0000

    Exception handling - findById
    Checklist:
     NEW CLASS: services.exceptions.ResourceNotFoundException
     NEW CLASS: resources.exceptions.StandardError
     NEW CLASS: resources.exceptions.ResourceExceptionHandler
     UserService

--- src/main/java/com/miguel/store/services/UserService.java
+++ src/main/java/com/miguel/store/services/UserService.java
@@ -2,6 +2,7 @@ package com.miguel.store.services;
 
 import com.miguel.store.entities.User;
 import com.miguel.store.repositories.UserRepository;
+import com.miguel.store.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
       Optional<User> obj = repository.findById(id);
-      return obj.get();
+      return obj.orElseThrow(()-> new ResourceNotFoundException(id));
     }
     public User insert(User obj){
         return repository.save(obj);
---
+++ src/main/java/com/miguel/store/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.miguel.store.services.exceptions;
+
+public class ResourceNotFoundException  extends RuntimeException{
+
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
