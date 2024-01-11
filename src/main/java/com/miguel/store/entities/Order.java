166a53793aa925da8fb4dfb5662cfa705bee9ca6
migue <miguelangelosilvacarvalho96@gmail.com>
2024-01-11 15:55:41 +0000

    OrderStatus enum

--- src/main/java/com/miguel/store/entities/Order.java
+++ src/main/java/com/miguel/store/entities/Order.java
@@ -2,6 +2,7 @@ package com.miguel.store.entities;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.miguel.store.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,6 +18,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss 'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -24,9 +27,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -54,6 +58,15 @@ public class Order implements Serializable {
         this.client = client;
     }
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
