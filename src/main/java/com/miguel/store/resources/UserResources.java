d9f48a953086c4bfce688bdd0124dfba3e33cb1d
migue <miguelangelosilvacarvalho96@gmail.com>
2024-01-11 21:42:04 +0000

    User insert
    Checklist:
     UserService
     UserResource

--- src/main/java/com/miguel/store/resources/UserResources.java
+++ src/main/java/com/miguel/store/resources/UserResources.java
@@ -4,11 +4,10 @@ import com.miguel.store.entities.User;
 import com.miguel.store.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.security.PublicKey;
 import java.util.List;
 
@@ -28,6 +27,13 @@ public class UserResources {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 
--- src/main/java/com/miguel/store/services/OrderService.java
+++ src/main/java/com/miguel/store/services/OrderService.java
@@ -23,4 +23,5 @@ public class OrderService {
       return obj.get();
     }
 
+
 }
